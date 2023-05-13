bad868f0fb98a2969b881ce866455c96ad6f201c
Jonibek Yarashev <yarashevjonibek@gmail.com>
2023-05-13 16:21:17 +0500

    done practical task1 Max number of unequal consecutive characters

---
+++ Max_Consecutive_Characters/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Max_Consecutive_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the string: ");
+            string line = Console.ReadLine();
+            int max = int.MinValue;
+            for(int i = 0; i < line.Length - 1; i++)
+            {
+                int current = 1;
+                if (line[i] != line[i + 1])
+                {
+                    current++;
+                    for(int j = i + 1; j < line.Length - 1; j++)
+                    {
+                        if (line[j] != line[j + 1])
+                            current++;
+                        else break;
+                    }
+                }
+                if (max < current)
+                    max = current;
+            }
+            Console.WriteLine("Max number of consecutive characters: " + max);
+        }
+    }
+}
