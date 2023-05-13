d8583a4aa31bb5503c1ba6f3586cc70868e70101
Jonibek Yarashev <yarashevjonibek@gmail.com>
2023-05-13 16:43:21 +0500

    done practical task2 convert decimal to new base number

---
+++ DecimalToBase/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace DecimalToBase
+{
+    class Prgram
+    {
+        static void Main(string[] args)
+        {
+            int number, baseNum, remainder;
+            string newNumber = string.Empty;
+            Console.Write("Enter the number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter base: ");
+            baseNum = Convert.ToInt32(Console.ReadLine());
+            while (number > 0)
+            {
+                remainder = number % baseNum;
+                number /= baseNum;
+                newNumber += remainder;
+            }
+            newNumber = new string(newNumber.Reverse().ToArray());
+            Console.WriteLine(newNumber);
+        }
+    }
+}
